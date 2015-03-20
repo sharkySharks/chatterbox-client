ac80b645e282dc78e51316187f646549f6088104
Sharky <kaydenalthen@gmail.com>
2015-03-20 08:56:24 -0500

    basic requirements passed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,123 +1,126 @@
 (function(){
 
-	window.app = {
-		server: 'https://api.parse.com/1/classes/chatterbox',
-		init: function(){
+  window.app = {
+    existingRooms:[],
+    server: 'https://api.parse.com/1/classes/chatterbox',
+    init: function(){
 
-		},
-		send: function(data){
-			alert("this is the alert from send: ", data)
-			$.ajax({
-			  url: app.server,
-			  type: 'POST',
-			  data: JSON.stringify(data),
-			  contentType: 'application/json',
-			  success: function (data) {
-			  	app.fetch()
-			    console.log('chatterbox: Message sent');
-			  },
-			  error: function (data) {
-			    console.error('chatterbox: Failed to send message');
-			  }
-			});
-		},
+    },
+    send: function(data){
+      $.ajax({
+        url: app.server,
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (data) {
+          app.fetch()
+          alert("success!")
+          console.log('chatterbox: Message sent');
+        },
+        error: function (data) {
+          console.error('chatterbox: Failed to send message');
+        }
+      });
+    },
 
-		fetch: function(){
-			// var ajaxData = data.reverse();
-			$.ajax({
-			  url: app.server,
-			  type: 'GET',
-			  data: {'order': '-createdAt'},
-			  contentType: 'application/json',
-			  success: function (data) {
-			  	console.log(data)
-			    for (var i=0;i<data.results.length;i++){
-			    	if (data.results[i].roomname===$("#roomSelect option:selected").text()){
-			    		app.addMessage(data.results[i])
-			    	}
-			    }
+    fetch: function(){
+      // var ajaxData = data.reverse();
+      $.ajax({
+        url: app.server,
+        type: 'GET',
+        data: {'order': '-createdAt'},
+        contentType: 'application/json',
+        success: function (data) {
+          console.log(data)
+          
+          app.populateMessages(data)
+          app.populateRooms(data)
+        
+        },
+        error: function (data) {
+          console.error('chatterbox: Failed to retrieve');
+        }
+      });
+    },
 
-			    var rooms=[]
+    populateMessages: function(data){
+      app.clearMessages()
 
-			    for (var i=0;i<data.results.length;i++){
-			    	rooms.push(data.results[i].roomname)
-			    }
+      for (var i=0;i<data.results.length;i++){
+            if (data.results[i].roomname===$("#roomSelect option:selected").text()){
+              app.addMessage(data.results[i])
+            }
+          }
+    },
 
-			    rooms=_.uniq(rooms)
+    clearMessages: function(message){
+      $("#chats").empty()
+    },
 
-			    for (var i=0;i<rooms.length;i++){
-			    	app.addRoom(rooms[i])
-			    }
+    addMessage: function(message){
+      var user = message.username
+      var chat = "<div class='chat'><div class='username "+user+"'>"+user+"</div><div class='text'>"+message.text+"</div></div>"
+      $("#chats").append(chat)
+    },
+
+    populateRooms:function(data){
+        var rooms=[]
+
+          for (var i=0;i<data.results.length;i++){
+            rooms.push(data.results[i].roomname)
+          }
+          
+          for (var i=0;i<rooms.length;i++){
+            if (app.existingRooms.indexOf(rooms[i]) ===-1){
+              app.addRoom(rooms[i])
+              app.existingRooms.push(rooms[i])            
+            }
+          }
+
+    },
+
+    addRoom: function(rooms){
+      $("#roomSelect").append("<option>"+rooms+"</option>")
+    },
+
+    addFriend: function(context){
+      var classHolder=$(context).attr('class').split(" ")
+      $("."+classHolder[1]).addClass("friend")
+    },
+
+    submitHandler: function(){
+
+      var userName = window.location.href.split('username=')[1];
+      var message = {
+        username: userName.slice(0,-1),
+        text:$("#message").val(),
+        roomname:$("#roomSelect option:selected").text()
+      }
+
+      alert(message)
+      app.send(message)
+    }
+  }
+
+app.fetch()
+setInterval(function(){app.fetch()},3000)
+
+  $(document).ready(function(){
 
 
+    $(document).on('click', '.username', function(){
+      var context=this
+      app.addFriend(context)
+    })
 
-			  },
-			  error: function (data) {
-			    console.error('chatterbox: Failed to retrieve');
-			  }
-			});
-		},
+    $(".submit").click(function(){
+      app.submitHandler() 
+    })
 
-		clearMessages: function(message){
-			$("#chats").empty()
-		},
-
-		addMessage: function(message){
-			var user = message.username
-			var box = "<div class='box'><div class='username "+user+"'>"+user+"</div><div class='text'>"+message.text+"</div></div>"
-			$("#chats").append(box)
-		},
-
-		addRoom: function(rooms){
-			$("#roomSelect").append("<option>"+rooms+"</option>")
-		},
-
-		addFriend: function(context){
-			var classHolder=$(context).attr('class').split(" ")
-			$("."+classHolder[1]).addClass("friend")
-		},
-
-		// handleSubmit: function(){
-		// 	var message = $("#message")
-		// 	app.send(message.val())
-		// }
-
-
-	}
-
-		app.fetch()
-	window.setInterval(function(){
-		app.clearMessages()
-		app.fetch()
-	},10000)
-
-	$(document).ready(function(){
-
-
-		// $(".username").click(function(){app.addFriend()})
-		$(document).on('click', '.username', function(){
-			var context=this
-			app.addFriend(context)
-		})
-
-		$(document).on('click', '.submit', function(){
-			var userName = window.location.href.split('username=')[1];
-			console.log(userName);
-			var message = {
-				username: userName.slice(0,-1),
-				text:$("#message").val(),
-				roomname:$("#roomSelect option:selected").text()
-			}
-			console.log("username:"+message.username)
-			console.log("text: "+ message.text)
-			console.log("roomname:" + message.roomname)
-			app.send(message)
-		})
-
-		
-	})
+    $("#roomSelect").change(function(){
+        app.fetch()
+      });
+    
+  })
 
 })();
-
-
-
